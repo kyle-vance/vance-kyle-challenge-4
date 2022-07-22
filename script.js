075cb26b402241c463057bcd11d772fa602b10ba
Kyle <kylevance801@gmail.com>
2022-07-22 10:32:09 -0600

    basic html, added some questions I pulled from w3 schools quiz

---
+++ script.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        question: "Which of the following is an example of a boolean?",
+        options: ["true", "92", "alerts", "hello"],
+        correctAnswer: "true"
+    },
+    {
+        question: "Which language is the most popular and in-demand language?",
+        options: ["html", "AWS", "python", "javascript"],
+        correctAnswer: "javascript"
+    },
+    {
+        question: "Javascript file is referenced in which HTML element?",
+        options: ["<head>", "<script>", "<javscript>", "<any of the above>"],
+        correctAnswer: "<script>"
+    },
+    {
+        question: "How do you find the number with the highest value of x and y?",
+        options: ["Math.ceil(x, y)", "top(x, y)", "ceil(x, y)", "Math.max(x, y)"],
+        correctAnswer: "Math.max(x, y)"
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        options: ["-", "+", "=", "*"],
+        correctAnswer: "="
+    },
+
+];
